1c5428a5fddd46b594a4e356c1394a61a45ce9e6
WindingTheRopes <17016045+windingtheropes@users.noreply.github.com>
2021-06-22 12:10:18 -0400

    add files
    add assetcache, packageindexcache, remove plugin, and work on the main module

--- roblox/src/server/rblxpm/init.lua
+++ roblox/src/server/rblxpm/init.lua
@@ -1,3 +1,17 @@
-local rbxpm = {
+local rblxpm = {}
+
+local cacheFolder = script.cache
+local packageIndexUrl = "https://raw.githubusercontent.com/rblxpm/rblxpm-packages/main/packageIndex.json"
+local httpService = game:GetService("HttpService")
+local packageIndex
+
+function rblxpm:init()
+    packageIndex = httpService:GetAsync(packageIndexUrl)
     
-}
+end
+
+function rblxpm:import(moduleName)
+   
+end
+
+return rblxpm
