584d53f2e126f7bb5ab49a18bcc979576928b5a1
WindingTheRopes <17016045+windingtheropes@users.noreply.github.com>
2021-06-23 14:23:08 -0400

    add instance and id require

--- roblox/src/server/rblxpm/init.lua
+++ roblox/src/server/rblxpm/init.lua
@@ -5,6 +5,7 @@ local importsCache = script.importsCache
 local packageIndexUrl = "https://raw.githubusercontent.com/rblxpm/rblxpm-packages/main/packageIndex.json"
 local HttpService = game:GetService("HttpService")
 local packageIndex = script.packageIndex
+local InsertService = game:GetService("InsertService")
 
 function rblxpm:init()
     
@@ -28,7 +29,44 @@ end
 
 function rblxpm:import(ref)
    if typeof(ref) == 'string' then
-   elseif typeof(ref) == ''
+        --RBLXPM name import
+   elseif typeof(ref) == 'number' then
+        --RBLXPM roblox id import
+        local asset
+        pcall(function()
+            asset = InsertService:LoadAsset(ref)
+        end)
+        if asset then
+            local mainModule
+            pcall(function()
+                --Main module doesnt have to be called mainmodule, but it has to exist.
+                mainModule = asset:FindFirstChildWhichIsA("ModuleScript")
+            end)
+            if mainModule then
+                return require(mainModule)
+            else
+                warn("[RBXPM] No main module found.")
+            end 
+        else
+            warn("[RBXPM] Error importing module: asset does not exist.")
+        end
+       
+   elseif typeof(ref) == 'Instance'then
+        if ref:IsA("ModuleScript") then
+            return require(ref)
+        else
+            local mainModule
+            pcall(function()
+                mainModule = ref:FindFirstChildWhichIsA("ModuleScript")
+            end)
+            if mainModule then
+                return require(mainModule)
+            else
+                warn("[RBLXPM] No main module found.")
+            end
+                
+        end
+   end 
 end
 
 return rblxpm
